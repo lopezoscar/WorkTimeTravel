37c168e37b01e6ce7cb2f89ac679e6bbfb3a73b2
Oscar <lopezoscar.job@gmail.com>
2015-06-20 15:55:53 -0300

    angular localforage

---
+++ WorkTimeTravelApp/www/directives/listHours/listHours.js
@@ -0,0 +1,19 @@
+var directives = angular.module('starter.directives');
+
+directives.directive('listHours',['HoursService',function(HoursService){
+
+    return {
+        restrict:'E'
+        ,templateUrl:'directives/listHours/listHours.html'
+        ,scope:{
+            hours:'='
+        }
+        ,controller:["$scope",function($scope){
+            $scope.removeHour = function(anHour){
+                console.log('removing hour',anHour);
+                HoursService.remove(anHour);
+                $scope.hours.splice($scope.hours.indexOf(anHour),1);
+            }
+        }]
+    }
+}]);
--- WorkTimeTravelApp/www/js/app.js
+++ WorkTimeTravelApp/www/js/app.js
@@ -5,79 +5,89 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ngMessages', 'starter.controllers', 'starter.services','starter.directives'])
+angular.module('starter', ['ionic','ngMessages',"firebase","LocalForageModule",'starter.controllers', 'starter.services','starter.directives'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
+    .config(['$localForageProvider', function($localForageProvider){
+      $localForageProvider.config({
+        driver      : 'localStorageWrapper', // if you want to force a driver
+        name        : 'WorkTimeFlights', // name of the database and prefix for your data, it is "lf" by default
+        version     : 1.0, // version of the database, you shouldn't have to use this
+        storeName   : 'hours', // name of the table
+        description : 'work hours'
+      });
+    }])
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleLightContent();
         }
-      }
+      });
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .config(function($stateProvider, $urlRouterProvider) {
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-});
+        // setup an abstract state for the tabs directive
+          .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+          })
+
+        // Each tab has its own nav history stack:
+
+          .state('tab.dash', {
+            url: '/dash',
+            views: {
+              'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+              }
+            }
+          })
+
+          .state('tab.chats', {
+            url: '/chats',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+              }
+            }
+          })
+          .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/chat-detail.html',
+                controller: 'ChatDetailCtrl'
+              }
+            }
+          })
+
+          .state('tab.account', {
+            url: '/account',
+            views: {
+              'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+              }
+            }
+          });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/dash');
+
+    });
