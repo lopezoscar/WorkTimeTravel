13c49c156a5f5453cb26b5ac0cff95311344c27e
Oscar <lopezoscar.job@gmail.com>
2015-06-20 16:57:44 -0300

    correcciones labels y layout

--- WorkTimeTravelApp/www/directives/loadHours/loadHours.js
+++ WorkTimeTravelApp/www/directives/loadHours/loadHours.js
@@ -1,5 +1,25 @@
 "use strict";
 var directives = angular.module('starter.directives', ['starter.services']);
+
+directives.directive('capitalize', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+            var capitalize = function(inputValue) {
+                if(inputValue == undefined) inputValue = '';
+                var capitalized = inputValue.toUpperCase();
+                if(capitalized !== inputValue) {
+                    modelCtrl.$setViewValue(capitalized);
+                    modelCtrl.$render();
+                }
+                return capitalized;
+            }
+            modelCtrl.$parsers.push(capitalize);
+            capitalize(scope[attrs.ngModel]);  // capitalize initial value
+        }
+    };
+});
+
 directives.directive('loadHours',['HoursService','$ionicSlideBoxDelegate',function(HoursService,$ionicSlideBoxDelegate){
 
     return {
